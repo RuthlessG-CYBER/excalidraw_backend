765fbf933f8bbc5bb1d2b465e21f0f091a9ed140
RuthlessG-CYBER <pandasoumya604@gmail.com>
2025-11-17 17:43:23 +0530

    savetoBackend

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URL = process.env.MONGODB_URL;
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+}));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
